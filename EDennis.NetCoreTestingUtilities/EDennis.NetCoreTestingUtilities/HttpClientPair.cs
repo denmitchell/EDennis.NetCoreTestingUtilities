f7252d321e06a9d23db9db034caec10fa5877057
denmitchell <den.mitchell@yahoo.com>
2018-08-09 10:38:51 -0400

    Added HttpClientPair to facilitate testing of solutions having both an Internal API and an External API, requiring two HttpClient instances.  Also, added a PortInspector class to find available ports on the test machine

---
+++ EDennis.NetCoreTestingUtilities/EDennis.NetCoreTestingUtilities/HttpClientPair.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+
+namespace EDennis.NetCoreTestingUtilities.Json {
+
+    /// <summary>
+    /// This class is analogous to the WebApplicationFactory class used
+    /// as a ClassFixture in unit testing; however, this class wraps
+    /// two TestServers (instead of one) to accommodate and internal
+    /// API and external API.
+    /// 
+    /// For Unit tests, see https://github.com/denmitchell/SampleApi
+    /// </summary>
+    public class HttpClientPair<TInternalStartup,TExternalStartup> : IDisposable 
+        where TInternalStartup : class 
+        where TExternalStartup : class
+        {
+
+        //clients for internal and external APIs
+        public static HttpClient internalClient;
+        public static HttpClient externalClient;
+
+
+        /// <summary>
+        /// Constructs a new HttpClientCollection with 
+        /// 
+        /// </summary>
+        public HttpClientPair() {
+
+            var ports = PortInspector.GetAvailablePorts(5000, 2);
+
+            //setup the internal server and client
+            TestServer internalServer = new TestServer(new WebHostBuilder()
+                .UseStartup<TInternalStartup>()
+                );
+            internalServer.BaseAddress = new Uri($"http://localhost:{ports[0]}/");
+            internalClient = internalServer.CreateClient();
+
+
+            //setup the external server and client
+            TestServer externalServer = new TestServer(new WebHostBuilder()
+                .UseStartup<TExternalStartup>()
+                //*** add reference to internal client as singleton
+                .ConfigureServices(services => {
+                    services.AddSingleton(internalClient);
+                    })
+                );
+            externalServer.BaseAddress = new Uri($"http://localhost:{ports[1]}/");
+            externalClient = externalServer.CreateClient();
+
+        }
+
+        /// <summary>
+        /// No need to dispose anything explicitly
+        /// </summary>
+        public void Dispose() { }
+    }
+
+}
