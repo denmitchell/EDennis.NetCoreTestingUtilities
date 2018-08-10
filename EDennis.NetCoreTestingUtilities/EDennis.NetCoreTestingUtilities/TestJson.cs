b7bd2f318a02bfe81e0a398bd62be85f50e40847
denmitchell <den.mitchell@yahoo.com>
2018-08-10 15:13:34 -0400

    Updated TestJson and TestJsonAttribute classes to work better with Xunit.

--- EDennis.NetCoreTestingUtilities/EDennis.NetCoreTestingUtilities/TestJson.cs
+++ EDennis.NetCoreTestingUtilities/EDennis.NetCoreTestingUtilities/TestJson.cs
@@ -1,8 +1,12 @@
-﻿using EDennis.NetCoreTestingUtilities.Extensions;
+﻿using Dapper;
+using EDennis.NetCoreTestingUtilities.Extensions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using Xunit.Abstractions;
 
 namespace EDennis.NetCoreTestingUtilities{
 
@@ -18,13 +22,14 @@ namespace EDennis.NetCoreTestingUtilities{
         public string TestCase { get; set; }
         public string TestFile { get; set; }
         public string Json { get; set; }
+
     }
 
     /// <summary>
     /// Provides TestJson records in a hierarchical result
     /// grouped by a particular TestCase
     /// </summary>
-    public class JsonTestCase {
+    public class JsonTestCase : IXunitSerializable {
         public string ProjectName { get; set; }
         public string ClassName { get; set; }
         public string MethodName { get; set; }
@@ -32,6 +37,21 @@ namespace EDennis.NetCoreTestingUtilities{
         public string TestCase { get; set; }
 
         public List<JsonTestFile> JsonTestFiles { get; set; }
+
+        public void Deserialize(IXunitSerializationInfo info) {
+            var obj = JsonConvert.DeserializeObject<JsonTestCase>(info.GetValue<string>("obj"));
+            ProjectName = obj.ProjectName;
+            ClassName = obj.ClassName;
+            MethodName = obj.MethodName;
+            TestScenario = obj.TestScenario;
+            TestCase = obj.TestCase;
+            JsonTestFiles = obj.JsonTestFiles;
+        }
+
+        public void Serialize(IXunitSerializationInfo info) {
+            var json = JsonConvert.SerializeObject(this);
+            info.AddValue("obj", json);
+        }
 
         public string GetJson(string testFile) {
             return JsonTestFiles.FirstOrDefault(f => f.TestFile == testFile).Json;
@@ -43,6 +63,58 @@ namespace EDennis.NetCoreTestingUtilities{
             return new T().FromJsonString(json);
         }
 
+        public static List<JsonTestCase> GetTestCasesForProject(string connectionString, string testJsonSchema, string testJsonTable, string projectName) {
+            var testCases = new List<JsonTestCase>();
+
+
+            using (var cxn = new SqlConnection(connectionString)) {
+                var testJson = cxn.Query<TestJson>(
+                    "select ProjectName, ClassName, MethodName, TestScenario, TestCase, TestFile, Json from "
+                    + $"{testJsonSchema}.{testJsonTable} where "
+                    + "ProjectName = @projectName",
+                    new { projectName });
+
+
+                if (testJson.Count() == 0) {
+                    throw new ArgumentException(
+                        $"No TestJson record found for ProjectName: {projectName}.");
+                }
+
+                //construct a JsonTestCase as the return object
+                var qry = testJson.GroupBy(
+                        r => new { r.ProjectName, r.ClassName, r.MethodName, r.TestScenario, r.TestCase },
+                        r => new JsonTestFile { TestFile = r.TestFile, Json = r.Json },
+                        (key, g) =>
+                            new JsonTestCase {
+                                ProjectName = key.ProjectName,
+                                ClassName = key.ClassName,
+                                MethodName = key.MethodName,
+                                TestScenario = key.TestScenario,
+                                TestCase = key.TestCase,
+                                JsonTestFiles = g.ToList()
+                            });
+
+                //return all objects
+                foreach (var rec in qry)
+                    testCases.Add(rec);
+
+                return testCases;
+            }
+
+        }
+
+        public static IEnumerable<object[]> GetDataForXUnit(List<JsonTestCase> TestCases,
+            string className, string methodName, string testScenario, string testCase) {
+
+            var qry = TestCases.Where(t => t.ClassName == className && t.MethodName == methodName
+                            && t.TestScenario == testScenario && t.TestCase == testCase);
+
+            //return all objects
+            foreach (var rec in qry.AsEnumerable())
+                yield return new object[] { $"{testScenario}({testCase})", rec };
+        }
+
+
     }
 
     /// <summary>
@@ -52,6 +124,9 @@ namespace EDennis.NetCoreTestingUtilities{
     public class JsonTestFile {
         public string TestFile { get; set; }
         public string Json { get; set; }
+
     }
+
+
 
 }
--- EDennis.NetCoreTestingUtilities/EDennis.NetCoreTestingUtilities/TestJsonAttribute.cs
+++ EDennis.NetCoreTestingUtilities/EDennis.NetCoreTestingUtilities/TestJsonAttribute.cs
@@ -15,66 +15,42 @@ namespace EDennis.NetCoreTestingUtilities
     /// </summary>
     public class TestJsonAttribute : DataAttribute {
 
+
+        public static List<JsonTestCase> TestCases { get; set; }
+
+
         private string _connectionString;
         private string _testJsonSchema;
         private string _testJsonTable;
         private string _projectName;
         private string _className;
         private string _methodName;
+        private string _testScenario;
+        private string _testCase;
 
-        /// <summary>
-        /// Constructs a new TestJson attribute with the given parameters
-        /// for the database and query criteria
-        /// </summary>
-        /// <param name="connectionString">SQL Server Connection String</param>
-        /// <param name="testJsonSchema">Schema name (e.g., _maintenance)</param>
-        /// <param name="testJsonTable">Table name</param>
-        /// <param name="projectName">Project name</param>
-        /// <param name="className">Class Name</param>
-        /// <param name="additionalCriteria">any additional criteria formatted as a SQL expression </param>
+
         public TestJsonAttribute(string connectionString, string testJsonSchema, string testJsonTable,
-            string projectName, string className, string methodName) {
+            string projectName, string className, string methodName, string testScenario, string testCase) {
+
+            TestCases = new List<JsonTestCase>();
 
             _connectionString = connectionString;
             _testJsonSchema = testJsonSchema;
             _testJsonTable = testJsonTable;
             _projectName = projectName;
-            _className = className;
-            _methodName = methodName;
-    }
-
-
-
-
-        /// <summary>
-        /// Returns an IEnumerable of JsonTestCase records, each of which
-        /// has been cast as an object[].  In the XUnit test, use a Theory
-        /// attribute and a TestJson attribute such as the following:
-        ///
-        /// [Theory]
-        /// [TestJson("Server=(localdb)\\mssqllocaldb;Database=tmp;Trusted_Connection=True;",
-        ///     "_maintenance", "TestJson", "P1", "C1", "M")]
-        /// 
-        /// Then, specify "JsonTestCase testCase" as the single parameter 
-        /// for the test method
-        /// </summary>
-        /// <param name="methodInfo"></param>
-        /// <returns></returns>
-        public override IEnumerable<object[]> GetData(MethodInfo methodInfo) {
+            
 
             using (var cxn = new SqlConnection(_connectionString)) {
                 var testJson = cxn.Query<TestJson>(
                     "select ProjectName, ClassName, MethodName, TestScenario, TestCase, TestFile, Json from "
                     + $"{_testJsonSchema}.{_testJsonTable} where "
-                    + "ProjectName = @_projectName and ClassName = @_className"
-                    + $" and MethodName = @_methodName ",
-                    new { _projectName, _className,  _methodName });
+                    + "ProjectName = @_projectName",
+                    new { _projectName });
 
 
                 if (testJson.Count() == 0) {
                     throw new ArgumentException(
-                        $"No TestJson record found for ProjectName: {_projectName}, " +
-                        $"ClassName: {_className}, MethodName: {_methodName}. ");
+                        $"No TestJson record found for ProjectName: {_projectName}.");
                 }
 
                 //construct a JsonTestCase as the return object
@@ -82,20 +58,35 @@ namespace EDennis.NetCoreTestingUtilities
                         r => new { r.ProjectName, r.ClassName, r.MethodName, r.TestScenario, r.TestCase },
                         r => new JsonTestFile { TestFile = r.TestFile, Json = r.Json },
                         (key, g) =>
-                            new JsonTestCase { ProjectName = key.ProjectName,
-                                    ClassName = key.ClassName, MethodName = key.MethodName,
-                                    TestScenario  = key.TestScenario,
-                                    TestCase = key.TestCase,
+                            new JsonTestCase {
+                                ProjectName = key.ProjectName,
+                                ClassName = key.ClassName,
+                                MethodName = key.MethodName,
+                                TestScenario = key.TestScenario,
+                                TestCase = key.TestCase,
                                 JsonTestFiles = g.ToList()
                             });
 
                 //return all objects
-                foreach (var rec in qry.AsEnumerable())
-                    yield return new object[] { rec };
+                foreach (var rec in qry)
+                    TestCases.Add(rec);
 
             }
+
         }
 
+
+        public override IEnumerable<object[]> GetData(MethodInfo methodInfo) {
+
+            var qry = TestCases.Where(t => t.ClassName == _className && t.MethodName == _methodName
+                && t.TestScenario == _testScenario && t.TestCase == _testCase);
+
+            //return all objects
+            foreach (var rec in qry.AsEnumerable())
+                yield return new object[] { $"{_testScenario}({_testCase})", rec };
+
+        }
     }
+
 
 }
